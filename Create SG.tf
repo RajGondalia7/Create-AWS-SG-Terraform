a9ae4a68baa550e15ba7b2d3206d2be8a4cf312a
Raj Gondalia <rmgondalia7@gmail.com>
2023-03-17 14:01:37 +0000

    test

---
+++ Create SG.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "var.region"
+  access_key = "YOUR_ACCESS_KEY"
+  secret_key = "YOUR_SECRET_KEY"
+}
+
+resource "aws_security_group" "test"{
+  name = "Raj"
+  
+  ingress {
+	description = "test"
+	from_port = 443
+	to_port = 443
+	protocol = "tcp"
+  }
+  
+  egress {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+}
